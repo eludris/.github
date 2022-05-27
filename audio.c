6ed115e9eede910c1db44d7aa16e333b0c0e241f
Enoki <enokiun@gmail.com>
2022-05-27 15:08:43 +0300

    Delete audio.c

--- audio.c
+++
@@ -1,9 +0,0 @@
-fn decode(value: u32) -> char {
-    char::from_u32(value >> 1).unwrap()
-}
-
-fn main() {
-    // We are no strangers to loooove.
-    // You know the rules and so do I!
-    // Now complete this code. 
-}
